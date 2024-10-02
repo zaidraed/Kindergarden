4ba31502d5bfbd09c6dcd7de0ef0f90f28157d8f
Zaid Raed <zaidraed@gmail.com>
2024-10-02 13:49:20 -0300

    Update api.js

--- Jardin La Estacion/src/api/api.js
+++ Jardin La Estacion/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://kindergarden-production.up.railway.app/api",
+  baseURL: "http://kindergarden-production.up.railway.app",
   headers: {
     "Content-Type": "application/json",
   },
