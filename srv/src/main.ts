2288dec1f7a2fac11728cdafc4f8b557e042f6ee
Zaid Raed <zaidraed@gmail.com>
2024-10-02 14:07:56 -0300

    Update main.ts

--- srv/src/main.ts
+++ srv/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from "@nestjs/core";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
