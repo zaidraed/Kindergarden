3aad04c16ccd8ec2f717dbb28e52e3c7a8dc6285
zaidraed <zaidraed@gmail.com>
2024-10-08 16:59:32 -0300

    deploy config

--- Jardin La Estacion/src/api/api.js
+++ Jardin La Estacion/src/api/api.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://kindergarden-production.up.railway.app",
   headers: {
     "Content-Type": "application/json",
   },
-
   withCredentials: true, // Esto asegura que las cookies se envíen automáticamente
 });
 
